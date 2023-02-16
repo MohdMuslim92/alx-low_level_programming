2bf002486c5b6fd7ff35c89722c0220ca0f9ba81
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-02-16 15:23:59 -0800

    Print a string with printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-/* main : starting the program */
+/**
+ * main - Entry point
+ * Description: print a string with printf() function
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
