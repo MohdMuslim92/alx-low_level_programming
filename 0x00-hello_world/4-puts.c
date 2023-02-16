e72fe188f665369624981f502c218e629550c415
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-02-16 15:19:26 -0800

    print a string with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-/* int  main : starting of the code which prints a simple string */
-/* return : 0 if successfull or 1 if there is an error */
+/**
+ * main - Entry point
+ *
+ * Description: Prin a string using puts() function
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {/* main : starting the program */
 	puts("\"Programming is like building a multilingual puzzle");
