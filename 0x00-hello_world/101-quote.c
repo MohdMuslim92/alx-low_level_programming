d009fc21a93c0988cee59bd0ccfa0259bf6cd362
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-02-16 16:16:14 -0800

    Print without printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,8 @@
  */
 int main(void)
 {
-	char my_string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDOUT_FILENO, my_string, strlen(my_string));
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDOUT_FILENO, msg, strlen(msg));
 	return (1);
 }
