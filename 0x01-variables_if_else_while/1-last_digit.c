635a8b9f1ef57173a58dc453139d68343c9b2fd8
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-02-17 10:58:54 -0800

    Get last digit of a random no

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,13 +18,14 @@ int main(void)
 	{
 		printf("%d and is greater than 5\n", n % 10);
 	}
-	else if (n % 10 < 6)
-	{
-		printf("%d and is less than 6 and not 0\n", n % 10);
-	}
 	if (n % 10 == 0)
 	{
 		printf("%d and is 0\n", n % 10);
+		return (0);
+	}
+	else if (n % 10 < 6)
+	{
+		printf("%d and is less than 6 and not 0\n", n % 10);
 	}
 	return (0);
 }
