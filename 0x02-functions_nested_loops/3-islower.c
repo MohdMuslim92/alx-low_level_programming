da3d18864ded83a45402b6781780da920f6d2211
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-02-21 09:48:12 -0800

    Function check if it's a lower case letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * _islower:check the character if it's lower or upper case
+ * the function take the parameter c
+ * c : parameter passed to the function
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	int i;
+
+	if (i >= 97 && i <= 122)
+	{
+		_putchar('0');
+		_putchar('\n');
+	} else
+	{
+		_putchar('0');
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -3,3 +3,4 @@
 * 0-putchar.c : print a string using a function that declared into a custom header file
 * 1-alphabet.c : print the alphabet letters with a function
 * 2-print_alphabet_x10.c : print the alphabet letters 10 times with a function
+* 3-islower.c : a function taht return 1 if the passed variable is a lower case letter otherwise it will return 0
