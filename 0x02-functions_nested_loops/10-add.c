409351613cd246dd53e500907650318acf40309d
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-02-21 18:23:48 -0800

    Add 2 numbers to each other

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * add : a function that adds two numbers to each other
+ * @n : parameter passed to the function
+ * @m : parameter passed to the function
+ * Return: Always 0 (Success)
+ */
+int add(int n, int m)
+{
+
+	return (n + m);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -10,3 +10,4 @@
 * 7-print_last_digit.c : return the last digit of a variable
 * 8-24_hours.c : print the minuts of the day
 * 9-times_table.c : a function that prints the 9 times table, starting with 0
+* 10-add.c : function to add two numbers to each other
