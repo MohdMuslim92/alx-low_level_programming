c9a93552944db1a95ec5c465373981d4ca14dec3
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-02-22 07:24:32 -0800

    Return the day and the remaining day of the year

--- 0x03-debugging/3-main.c
+++ 0x03-debugging/3-main.c
@@ -13,6 +13,27 @@ int main(void)
     int day;
     int year;
 
+    month = 9;
+    day = 30;
+    year = 2004;
+
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+    day = convert_day(month, day);
+
+    print_remaining_days(month, day, year);
+
+    month = 11;
+    day = 31;
+    year = 1930;
+
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+    day = convert_day(month, day);
+
+    print_remaining_days(month, day, year);
+
+
     month = 2;
     day = 29;
     year = 2000;
@@ -23,5 +44,24 @@ int main(void)
 
     print_remaining_days(month, day, year);
 
+    month = 4;
+    day = 01;
+    year = 1997;
+
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+    day = convert_day(month, day);
+
+    print_remaining_days(month, day, year);
+
+    month = 6;
+    day = 31;
+    year = 359;
+
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+    day = convert_day(month, day);
+
+    print_remaining_days(month, day, year);
     return (0);
 }
