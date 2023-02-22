0239ca10b5633c89f6c5548b7c36355f8de22d53
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-02-22 11:33:07 -0800

    First 98 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: a program that prints first 50 fibonacci numbers
+ * fibonacci starts from 0,1 and followed by adding the last two
+ * numbers in the sequence
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2;
+
+	while (i < 98)
+	{
+		if (i == 0)
+			printf("%ld", j);
+		else if (i == 1)
+			printf(", %ld", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %ld", k);
+		}
+
+		++i;
+	}
+
+	printf("\n");
+
+	return (0);
+}
