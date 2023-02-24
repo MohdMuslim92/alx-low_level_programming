c62271bbf4cd562e98fe1e2169a5501b1d7ae476
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-02-24 12:49:58 -0800

    Print positive and negative

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,72 +1,25 @@
 #include "main.h"
-
 /**
- * print_positive - Entry point
- * print_positive: using a function called print_positive to print
- * positive numbers without usin standard output functions
- * print_number: starting function
- * print_number: using a function called print_number to print
- * negative numbers without usin standard output functions
- * @n : parameter passed to the function
- */
-void print_positive(int n)
-{
-	if (n >= 0)
-	{
-		if (n > 9 && n < 100)
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-		} else if (n > 99 && n < 1000)
-		{
-			_putchar((n / 100) + '0');
-			_putchar((n % 100) / 10 + '0');
-			_putchar((n % 10) + '0');
-		} else if (n > 999 && n < 10000)
-		{
-			_putchar((n / 1000) + '0');
-			_putchar((n / 100) % 10 + '0');
-			_putchar((n % 100) / 10 + '0');
-			_putchar((n % 10) + '0');
-		} else
-		{
-			_putchar(n + '0');
-		}
-	}
-}
-
+  * print_number - function prints a positive and negative numbers
+  * @n: value passed to the function
+  *
+  * Return: Nothing!
+  */
 void print_number(int n)
 {
-	print_positive(n);
+	unsigned int i = n;
 
 	if (n < 0)
 	{
-		int x = -n;
-		if (x > 9 && x < 100)
-		{
-			_putchar('-');
-			_putchar((x / 10) + '0');
-			_putchar((x % 10) + '0');
-		} else if (x > 99 && x < 1000)
-		{
-			_putchar('-');
-			_putchar((x / 100) + '0');
-			_putchar((x % 100) / 10 + '0');
-			_putchar((x % 10) + '0');
-		} else if (x > 999 && x < 10000)
-		{
-			_putchar('-');
-			_putchar((x / 1000) + '0');
-			_putchar((x / 100) % 10 + '0');
-			_putchar((x % 100) / 10 + '0');
-			_putchar((x % 10) + '0');
-		} else
-		{
-			_putchar('-');
-			_putchar(x + '0');
-		}
+		i = -n;
+		_putchar('-');
 	}
 
+	i /= 10;
+
+	if (i != 0)
+		print_number(i);
+
+	_putchar((unsigned int) n % 10 + '0');
 
 }
-
