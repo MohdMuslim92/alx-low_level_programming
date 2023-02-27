82e4ca69497e0e7dcd24142e534080f8796edf8a
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-02-27 12:32:35 -0800

    Fixed-get the length of string first

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,15 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i, j;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		;
+	}
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
 	}
 	_putchar('\n');
 }
