2b8a339cdd1405ef47caebd4338aa0c730aa378f
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-03-02 14:29:05 -0800

    Capitalize first letter

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizeing function
+ * Description: a function that capitalize
+ * all first letters of all words in a string
+ * @str: string passed to the function
+ * Return: return a string with first letters capital
+ */
+char *cap_string(char *str)
+{
+	int i = 0, j = 11;
+	char a[] = {'.', ',', ';', ' ', '!', 9, 10, '?', '"', '(', ')', '{', '}'};
+
+	while (*(str + i) != '\0')
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if ((a[j] == *(str + i)) && (*(str + i + 1) >= 97) && (*(str + i + 1) <= 122))
+			{
+				*(str + i + 1) = *(str + i + 1) - 32;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
+
