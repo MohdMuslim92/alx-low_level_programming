e816a3b41989b9958bc82f69463087e5b2a6184e
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-03-02 10:59:16 -0800

    Encode string with ROT13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+*  rot13 - incoding function
+ * Description: a functi0n that encode a string by adding
+ * 13 character for every letter
+ * @str: string passed to the function
+ * Return: return an encoded string
+ */
+char *rot13(char *str)
+{
+	int i = 0, ai;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(str + i) != '\0')
+	{
+		for (ai = 0; ai < 52; ai++)
+		{
+			if ((*(str + i) == a[ai]))
+			{
+				*(str + i) = b[ai];
+			}
+		}
+		i++;
+	}
+
+	return (str);
+}
+
