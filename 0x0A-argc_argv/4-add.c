201fa006b0954a9c7dd775dfc41b5129b0af7d83
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-03-10 13:29:24 -0800

    Revert "Edited-add numbers"
    This reverts commit 471f716ea6c5f9357ea3cee757ccefb437cc20d7.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,23 +9,28 @@
  * @argv: array of command line arguments passed to the main
  * Return: return 0 (success)
  */
+
 int main(int argc, char *argv[])
 {
 	int i, total = 0;
 
-	if (argc < 1)
-		return (0);
-
-	for (i = 1; i < argc; i++)
+	if (argc >= 1)
 	{
-		if (!atoi(argv[i]))
+		for (i = 1; i < argc; i++)
 		{
-			printf("%s\n", "Error");
-			return (1);
+			if (atoi(argv[i]) && atoi(argv[i]) >= 0)
+			{
+				total += atoi(argv[i]);
+			} else
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
 		}
-		total += atoi(argv[i]);
+		printf("%d\n", total);
+	} else
+	{
+		printf("0\n");
 	}
-	printf("%d\n", total);
-
 	return (0);
 }
