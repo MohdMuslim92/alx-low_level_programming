bfbad41dad8835c35c071cc7d202fc900ec08a5d
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-03-10 13:01:15 -0800

    Add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - add numbers
+ * Description: a program that adds positive numbers
+ * @argc: number of command line arguments passed to the main
+ * @argv: array of command line arguments passed to the main
+ * Return: return 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, total = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]))
+			{
+				total += atoi(argv[i]);
+			} else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", total);
+	} else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
