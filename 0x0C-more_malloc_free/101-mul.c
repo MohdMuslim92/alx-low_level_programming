d281cdc38dd7fc1c0612edcb5e14c117d3d7eb94
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-03-16 16:52:11 -0700

    Program to multiply 2 numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - multiply 2 numbers
+ * Description: a program that multyply 2 positive numbers
+ * @argc: number of command line arguments passed to the main
+ * @argv: array of command line arguments passed to the main
+ * Return: return 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	int i, j;
+	long double total = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				exit (98);
+			}
+		}
+	}
+	total = atoi(argv[1]) * atoi(argv[2]);
+	printf("%.0Lf\n", total);
+	return (0);
+}
+
