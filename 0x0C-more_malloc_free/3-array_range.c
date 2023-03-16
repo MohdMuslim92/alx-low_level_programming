f2d195904ca66faa55af49b975bf5a1e09f6c403
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-03-16 11:57:32 -0700

    Function to allocate a ranged array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_range - create array of integers
+ * Description : a function that creates an
+ * array of integers values from min (included)
+ * to max (included), ordered from min to max
+ * @min: The starting number of the array
+ * @max: The last number of the array
+ * @: a pointer to the array
+ * Return: a pointer to the array or NULL if fails
+ */
+int *array_range(int min, int max)
+{
+	int *arr = NULL;
+	int i, range;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	range = max - min + 1;
+	arr = (int *)malloc(range * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
+		else
+	{
+		for (i = min; i <= max; i++)
+		{
+			arr[i] = i;
+		}
+		return (arr);
+	}
+}
+
