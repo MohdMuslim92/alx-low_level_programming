c5c2357f583f94d5a6cb2d7bbf06e0f021d24188
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-03-20 09:56:48 -0700

    Progtam prints the file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print file's name
+ * Description: a program that prints the name,
+ * followed by a new line
+ * Return: return 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
