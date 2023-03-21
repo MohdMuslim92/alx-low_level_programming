9922689067573d966796a53da1eb84f3ad7deee8
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-03-20 18:13:03 -0700

    Fixed-NULL pointer + age is 0 or less

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,16 +10,15 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
-		d = malloc(sizeof(struct dog));
-		if (d == NULL)
-			return;
-	}
+		return;
 	if (d->name == NULL)
 		printf("Name: (nil)");
 	else
 	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
+	if (d->age <= 0)
+		printf("Age: (nil)");
+	else
+	printf("Age: %.6f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nil)");
 	else
