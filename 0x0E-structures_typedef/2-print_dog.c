fd2d31c51df3d898b726586331f3ef5545d5ff41
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-03-20 18:32:22 -0700

    Fixed-print minimum age if null

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,15 +12,13 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("Name: (nil)");
-	else
-	printf("Name: %s\n", d->name);
+		d->name = "(nil)";
 	if (d->age <= 0)
-		printf("Age: (nil)");
-	else
-	printf("Age: %.6f\n", d->age);
+		printf("Age: (nil)\n");
 	if (d->owner == NULL)
-		printf("Owner: (nil)");
-	else
+		d->owner = "(nil)";
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
 	printf("Owner: %s\n", d->owner);
 }
