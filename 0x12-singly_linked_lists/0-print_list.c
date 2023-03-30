3cd69ccd695ae32a4e7176af62523a11cd5ceeb4
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-03-30 05:24:13 -0700

    Edited-print nil if str is null

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,6 +19,9 @@ size_t print_list(const list_t *h)
 	}
 	while (node != NULL)
 	{
+		if (node->str == NULL)
+		printf("[0] (nil)\n");
+		else
 		printf("[%u] %s\n", node->len, node->str);
 		node = node->next;
 		count++;
