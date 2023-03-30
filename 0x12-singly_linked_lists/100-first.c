17d1e152a5ca5d4bd57e5384a482dd2c10334fe5
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-03-30 15:30:45 -0700

    Function executes befor main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * pre_print - print function
+ * DESCRIPTION: a function that executed and print
+ * before main function executed
+ */
+void pre_print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
