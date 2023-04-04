696b8aa06f78985d8cc4e0997d36795086eb0213
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-04-03 18:21:13 -0700

    Function that frees allocated linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * free_listint - free allocated function
+ * DESCRIPTION: a function that frees a linked list
+ * @head: pointer to node passed to the function
+ * Return: 0 for sucess
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *node = head;
+	listint_t *next;
+
+	while (node != NULL)
+	{
+		next = node->next;
+		free(node);
+		node = next;
+	}
+}
