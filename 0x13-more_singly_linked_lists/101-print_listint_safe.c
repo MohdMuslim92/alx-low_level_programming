6bf3990a49327e05db1c30753c1e0b5a3ba67ada
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-04-04 17:00:48 -0700

    Function to print a linked list with while loop

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * print_listint_safe - print function
+ * DESCRIPTION: a function that prints a listint_t
+ * linked list with a while loop
+ * @head: pointer to node passed to the function
+ * Return: number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	const listint_t *node, *temp;
+
+	node = head;
+	while (node != NULL)
+	{
+		count++;
+		printf("[%p] %d\n", (void *)node, node->n);
+		temp = node;
+		node = node->next;
+		if (temp <= node)
+		{
+			printf("-> [%p] %d\n", (void *)node, node->n);
+			exit(98);
+		}
+	}
+	return (count);
+}
