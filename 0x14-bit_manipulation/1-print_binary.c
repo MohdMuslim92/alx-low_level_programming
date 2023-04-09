a573812be106aebdd33e9c7ee5cd38ff40bad188
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-04-09 08:06:51 -0700

    Fixed-if n is more than 32bits

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,16 +4,16 @@
 #include "main.h"
 /**
  * print_binary - convert function
- * DESCRIPTION: a function that convertsa decimal
+ * DESCRIPTION: a function that converts a decimal
  * number to a binary
  * @n: number to be converted
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int mask = 1 << 31;
+	unsigned long int mask = 1UL << 63;
 	int i, flag = 0;
 
-	for (i = 0; i < 32; i++)
+	for (i = 0; i < 64; i++)
 	{
 		if (n & mask)
 		{
@@ -23,13 +23,14 @@ void print_binary(unsigned long int n)
 		{
 			if (flag)
 			{
-			_putchar('0');
+				_putchar('0');
 			}
 		}
-		n <<= 1;
+		mask >>= 1;
 	}
-		if (!flag)
-		{
-			_putchar('0');
-		}
+	if (!flag)
+	{
+		_putchar('0');
+	}
 }
+
