400be5d566070560fef90b61d7a1081891ff0386
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-04-09 11:21:44 -0700

    Fixed-empty file or white spaces or write fail

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <ctype.h>
 #include "main.h"
+
+/**
+ * is_regular_file - check function
+ * DESCRIPTION: a function to check if the file is a
+ * regular file
+ * @filename: pointer to the file passed to the function
+ * Return: 1 if the file is a regular file, and 0 otherwise
+ */
+int is_regular_file(const char *filename)
+{
+	struct stat st;
+
+	if (stat(filename, &st) == -1)
+		return (0);
+	return (S_ISREG(st.st_mode));
+}
+
+/**
+ * read_file - read function
+ * DESCRIPTION: a function to read a file and return it's contents
+ * @filename: pointer to the file passed to the function
+ * @max_size: the maximum number of bytes to be read from the file
+ * @count:  pointer to a variable that will hold the number of bytes
+ * actually read from the file
+ * Return: a pointer to a character array that contains the data read
+ * from the file
+ */
+char *read_file(const char *filename, size_t max_size, ssize_t *count)
+{
+	FILE *fp;
+	char *buf;
+
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+		return (0);
+
+	if (!is_regular_file(filename))
+	{
+		fclose(fp);
+		return (NULL);
+	}
+
+	buf = malloc(sizeof(char) * (max_size + 1));
+	if (buf == NULL)
+	{
+		fclose(fp);
+		return (0);
+	}
+
+	*count = fread(buf, sizeof(char), max_size, fp);
+	if (*count == -1)
+	{
+		free(buf);
+		fclose(fp);
+		return (0);
+	}
+
+	buf[*count] = '\0';
+	fclose(fp);
+	return (buf);
+}
+
 /**
  * read_textfile - file read function
  * DESCRIPTION: a function that reads a text file
@@ -11,40 +75,22 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t count = 0;
-	char *size = malloc(sizeof(char) * (letters + 1));
-	FILE *fp;
+	ssize_t count;
+	char *size;
 
-	if (filename == NULL)
+	if (filename == NULL || *filename == '\0' || isspace(*filename))
 		return (0);
 
+	size = read_file(filename, letters, &count);
 	if (size == NULL)
 		return (0);
 
-	fp = fopen(filename, "r");
-	if (fp == NULL)
-	{
-		free(size);
-		return (0);
-	}
-
-	count = fread(size, sizeof(char), letters, fp);
-	if (count == -1)
-	{
-		free(size);
-		fclose(fp);
-		return (0);
-	}
-
-	size[count] = '\0';
 	if (fwrite(size, sizeof(char), (size_t)count, stdout) != (size_t)count)
 	{
 		free(size);
-		fclose(fp);
 		return (0);
 	}
 
-	fclose(fp);
 	free(size);
 
 	return (count);
