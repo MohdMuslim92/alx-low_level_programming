10fc944aa19955a74d4c19536169f715b06a8313
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-04-10 15:44:01 -0700

    Edited-use dprintf instead of fprintf

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
+
 int copy_file(const char *from_path, const char *to_path);
 
 /**
@@ -19,13 +20,14 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		fprintf(stderr, "Usage: cp file_from file_to\n");
-		return (97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 	result = copy_file(argv[1], argv[2]);
 
 	return (result);
 }
+
 /**
  * copy_file - copy file to file
  * DESCRIPTION: a function that copies the content of a file
@@ -39,58 +41,58 @@ int copy_file(const char *from_path, const char *to_path)
 	FILE *from = fopen(from_path, "r");
 	FILE *to = fopen(to_path, "w");
 	char buffer[1024];
-	int write, count, close, stat_file;
+	size_t count;
+	int write, close, stat_file;
 	struct stat st;
 
 	if (from == NULL)
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", from_path);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_path);
 		exit(98);
 	}
 
 	if (to == NULL)
 	{
-		fprintf(stderr, "Error: Can't write to %s\n", to_path);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to_path);
 		fclose(from);
 		exit(99);
 	}
+
 	if (access(to_path, F_OK) != -1)
 	{
 		if (truncate(to_path, 0) != 0)
 		{
-			fprintf(stderr, "Error: Can't truncate %s\n", to_path);
+			dprintf(STDERR_FILENO, "Error: Can't truncate %s\n", to_path);
 			fclose(from);
 			fclose(to);
 			exit(99);
 		}
 	}
 
-	while (!feof(from))
+	while ((count = fread(buffer, 1, sizeof(buffer), from)))
 	{
-		count = fread(buffer, 1, sizeof(buffer), from);
-		if (count > 0)
+		write = fwrite(buffer, 1, count, to);
+		if (write < 0 || (size_t)write < count)
 		{
-			write = fwrite(buffer, 1, count, to);
-			if (write < 0 || write < count)
-			{
-				fprintf(stderr, "Error: Can't write to %s\n", to_path);
-				fclose(from);
-				fclose(to);
-				exit(99);
-			}
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to_path);
+			fclose(from);
+			fclose(to);
+			exit(99);
 		}
 	}
+
 	close = fclose(from);
 	if (close != 0)
 	{
-		fprintf(stderr, "Error: Can't close fd %d\n", fileno(from));
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileno(from));
 		fclose(to);
 		exit(100);
 	}
+
 	close = fclose(to);
 	if (close != 0)
 	{
-		fprintf(stderr, "Error: Can't close fd %d\n", fileno(to));
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileno(to));
 		exit(100);
 	}
 
@@ -102,7 +104,7 @@ int copy_file(const char *from_path, const char *to_path)
 		mode |= S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 		if (chmod(to_path, mode) != 0)
 		{
-			fprintf(stderr, "Error: Can't set permissions for %s\n", to_path);
+			dprintf(STDERR_FILENO, "Error: Can't set permissions for %s\n", to_path);
 			return (-1);
 		}
 	}
