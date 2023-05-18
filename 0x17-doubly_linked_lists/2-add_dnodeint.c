63874f0fe88d44dbd8b6c31d22a1755c6c8deeb1
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-05-18 14:49:58 -0700

    Fixed - return NULL if pointer passed is NULL, if the new node is the first one it should be the head and prev point to NULL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,14 +14,19 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *node;
 
+	if (head == NULL)
+		return (NULL);
 	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
 	{
 		return (NULL);
 	}
 	node->n = n;
-	node->next = *head;
 	node->prev = NULL;
+	node->next = *head;
+	if (*head != NULL)
+		(*head)->prev = node;
+
 	*head = node;
 
 	return (node);
