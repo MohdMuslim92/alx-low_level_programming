409333e238b60573343c49bb9113b1efe5752664
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-09-13 16:23:57 +0300

    Update search_algos.h
    - Add singly linked list data structure
    - Add jump_list() function definition

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,6 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+*/
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
@@ -10,4 +27,5 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int advanced_binary_recursive(int *array, int low, int high, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 #endif
