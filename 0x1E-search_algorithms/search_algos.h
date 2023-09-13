62a6e0eafcb2eb685060c12f92f9a273845eb5e1
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-09-13 16:11:46 +0300

    Update search_algos.h
    - Add advanced_binary_recursive() function definition

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,4 +9,5 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+int advanced_binary_recursive(int *array, int low, int high, int value)
 #endif
